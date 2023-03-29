a3cfbcd182246029e0a0f1d05a7852b207fa7094
rivara <ricardo.v.ramirez@gmail.com>
2023-03-29 20:39:27 +0200

    #Instalacion laravel mix

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # LARAVEL_API_1
+<h1></h1>
+<ul>
+    <li> Composer y estructura PSR-4 en el proyecto </li>
+    <li>La última versión estable de Symfony/Laravel </li>
+    <li>Testing unitario</li>
+    <li>La API debe devolver y consumir los datos en formato JSON</li>
+    <li>Programar en el idioma inglés </li>
+    <li>Uso de herramientas de análisis estático (por ejemplo PHPStan en modo máximo) y de
+        estilo de código (por ejemplo PHP CS Fixer en modo @Symfony) </li>
+    <li> Uso de SCSS y Webpack, ya sea usando Webpack directamente o mediante Symfony
+    Encore / Laravel Mix o similar </li>
+    <li> Ofrecer un Swagger/OpenAPI para la parte de la </li>
+</ul>
---
+++ resources/views/index.blade.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+        <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous"> -->
+        <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.bundle.min.js" integrity="sha384-qKXV1j0HvMUeCBQ+QVp7JcfGl760yU08IQ+GpUo5hlbpg51QRiuqHAJz8+BrxE/N" crossorigin="anonymous"></script> -->
+    </head>
+    <body>
+     <h1>Prueba</h1>
+     <button type="button" class="btn btn-primary">Primary</button>
+        <table class="table" id="prueba">
+            <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">First</th>
+                    <th scope="col">Last</th>
+                    <th scope="col">Handle</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                <th scope="row">1</th>
+                    <td>Mark</td>
+                    <td>Otto</td>
+                    <td>@mdo</td>
+                </tr>
+                <tr>
+                <th scope="row">2</th>
+                    <td>Jacob</td>
+                    <td>Thornton</td>
+                    <td>@fat</td>
+                </tr>
+                <tr>
+                <th scope="row">3</th>
+                    <td>Larry</td>
+                    <td>the Bird</td>
+                    <td>@twitter</td>
+                </tr>
+            </tbody>
+        </table>
+    </body>
+</html>
+<script src="{{ asset('js/app.js') }}"></script>
