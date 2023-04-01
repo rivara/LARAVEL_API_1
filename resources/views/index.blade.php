9e8ab62af1e942cfe21e86a6007359541a00c234
rivara <ricardo.v.ramirez@gmail.com>
2023-04-01 11:43:02 +0200

    #integración final de laravel mix

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,16 +3,13 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
         <title>Laravel</title>
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-        <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous"> -->
-        <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.bundle.min.js" integrity="sha384-qKXV1j0HvMUeCBQ+QVp7JcfGl760yU08IQ+GpUo5hlbpg51QRiuqHAJz8+BrxE/N" crossorigin="anonymous"></script> -->
     </head>
     <body>
      <h1>Prueba</h1>
      <button type="button" class="btn btn-primary">Primary</button>
-        <table class="table" id="prueba">
+        <table class="table">
             <thead>
                 <tr>
                     <th scope="col">#</th>
